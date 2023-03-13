bc09535dd06bddd95fe9f1044fad40f8e9c28a3f
Steven Yang <stevenwaterloo2015@gmail.com>
2023-03-13 19:49:35 -0400

    safe args

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -22,7 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
@@ -34,9 +34,10 @@ class GameOverFragment : Fragment() {
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_game_over, container, false
         )
-        binding.tryAgainButton.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_gameOverFragment_to_gameFragment)
-        )
+        binding.tryAgainButton.setOnClickListener {
+            it.findNavController()
+                .navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
+        }
         return binding.root
     }
 }
